de83acdc6c49bf9dae3a7a1db7230cd4f42bf694
Jonas Cardoso <jonascard60@gmail.com>
2018-12-22 15:30:54 -0200

    Update AboutFragment.java

--- app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/other/AboutFragment.java
@@ -112,7 +112,7 @@ public class AboutFragment extends RecyclerViewFragment {
         sourcecode.setOnItemClickListener(new RecyclerViewItem.OnItemClickListener() {
             @Override
             public void onClick(RecyclerViewItem item) {
-                Utils.launchUrl("https://github.com/SmartPack/SmartPack-Kernel-Manager", requireActivity());
+                Utils.launchUrl("https://github.com/JonasCardoso/Floppy-Manager", requireActivity());
             }
         });
 
@@ -132,7 +132,7 @@ public class AboutFragment extends RecyclerViewFragment {
         donatetome.setOnItemClickListener(new RecyclerViewItem.OnItemClickListener() {
             @Override
             public void onClick(RecyclerViewItem item) {
-                Utils.launchUrl("https://www.paypal.me/sunilpaulmathew", getActivity());
+                Utils.launchUrl("https://www.paypal.me/floppykernel", getActivity());
             }
         });
 
@@ -174,7 +174,7 @@ public class AboutFragment extends RecyclerViewFragment {
             rootView.findViewById(R.id.image).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Utils.launchUrl("https://github.com/sunilpaulmathew", getActivity());
+                    Utils.launchUrl("https://github.com/JonasCardoso", getActivity());
                 }
             });
             return rootView;
