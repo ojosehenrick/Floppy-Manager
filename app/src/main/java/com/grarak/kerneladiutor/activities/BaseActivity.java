eaae7e70371ad966ad12be31b723a1d6ee21958c
Jonas Cardoso <jonascard60@gmail.com>
2018-12-22 21:54:28 -0200

    Theme: switching to green accent color

--- app/src/main/java/com/grarak/kerneladiutor/activities/BaseActivity.java
+++ app/src/main/java/com/grarak/kerneladiutor/activities/BaseActivity.java
@@ -81,7 +81,7 @@ public abstract class BaseActivity extends AppCompatActivity {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         Utils.DARK_THEME = Prefs.getBoolean("darktheme", false, this);
         int theme;
-        String accent = Prefs.getString("accent_color", "blue_accent", this);
+        String accent = Prefs.getString("accent_color", "green_accent", this);
         if (Utils.DARK_THEME) {
             theme = sAccentDarkColors.get(accent);
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
